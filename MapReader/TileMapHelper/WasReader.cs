a7162e156a2c0a6b8b31c00040277f213a4e762e
chenq <chenqi@sunwayworld.com>
2022-05-06 17:25:50 +0800

    写人物像素

--- MapReader/TileMapHelper/WasReader.cs
+++ MapReader/TileMapHelper/WasReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.IO;
 using System.Text;
 
@@ -8,6 +9,20 @@ namespace MapReader.TileMapHelper
     {
         private static String WAS_FILE_FLAT = "SP";
         private static int TCP_HEADER_SIZE = 12;
+
+        private const int TYPE_ALPHA = 0x00;// 前2位
+
+        private const int TYPE_ALPHA_PIXEL = 0x20;// 前3位 0010 0000
+
+        private const int TYPE_ALPHA_REPEAT = 0x00;// 前3位
+
+        private const int TYPE_FLAG = 0xC0;// 2进制前2位 1100 0000
+
+        private const int TYPE_PIXELS = 0x40;// 以下前2位 0100 0000
+
+        private const int TYPE_REPEAT = 0x80;// 1000 0000
+
+        private const int TYPE_SKIP = 0xC0; // 1100 0000
 
         private ushort[] palette;
 
@@ -80,10 +95,103 @@ namespace MapReader.TileMapHelper
                         {
                             lineOffsets[l] = FileByteReaderUtil.ReadInt32(fs);
                         }
+
+                        // 绘制图片
+                        int[] pixels = this.ParsePixels(fs, offset, lineOffsets, frameWidth, frameHeight, headerSize);
+
+
                     }
                 }
             }
 
+        }
+
+        private void CreateImage(int frameX, int frameY, int[] pixels, int width, int height) {
+            Mat textMat = new Mat(height, width, MatType.CV_8UC4, new Scalar(0, 0, 0, 0));
+            // TODO 写入像素
+
+            textMat.SaveImage(@"E:\test\tes\decode\player.png");
+        }
+
+        private int[] ParsePixels(FileStream fs, int frameOffset, int[] lineOffsets, int frameWidth, int frameHeight, int headerSize)
+        {
+            int[] pixels = new int[frameHeight * frameWidth];
+
+            int b, x, c;
+            int index;
+            int count;
+
+            for (int y = 0; y < frameHeight; y++)
+            {
+                x = 0;
+                fs.Seek(lineOffsets[y] + frameOffset + headerSize + 4, SeekOrigin.Begin);
+                while (x < frameWidth) {
+                    b = fs.ReadByte();
+                    switch ((b & TYPE_FLAG))
+                    {
+                        case TYPE_ALPHA:
+                            if ((b & TYPE_ALPHA_PIXEL) > 0)
+                            {
+                                index = fs.ReadByte();
+                                c = palette[index];
+                            }
+                            else if (b != 0)
+                            {
+                                count = b & 0x1F;
+                                b = fs.ReadByte();
+                                index = fs.ReadByte();
+                                c = palette[index];
+
+                                for (int i = 0; i < count; i++)
+                                {
+                                    pixels[y * frameWidth + x++] = c + ((c & 0x1F) << 16);
+                                }
+                            }
+                            else
+                            {
+                                if (x > frameWidth)
+                                {
+                                    Console.Error.WriteLine("block end error: [{0}][{1} / {2}]", y, x, frameWidth);
+                                    continue;
+                                }else if (x == 0)
+                                {
+                                    Console.Error.WriteLine("x == 0");
+                                }else
+                                {
+                                    x = frameWidth;
+                                }
+                            }
+                            break;
+                        case TYPE_PIXELS:
+                            count = b & 0x3F;
+                            for (int i = 0; i < count; i++)
+                            {
+                                index = fs.ReadByte();
+                                pixels[y * frameWidth + x++] = palette[index] + (0x1F << 16);
+                            }
+                            break;
+                        case TYPE_REPEAT:
+                            count = b & 0x3F;
+                            index = fs.ReadByte();
+                            c = palette[index];
+
+                            for (int i = 0; i < count; i++)
+                            {
+                                pixels[y * frameWidth + x++] = c + (0x1F << 16);
+                            }
+                            break;
+                        case TYPE_SKIP:
+                            count = b & 0x3F;
+                            x += count;
+                            break;
+                    }
+                }
+                if(x > frameWidth) {
+                    Console.Error.WriteLine("block end error: [{0}][{1} / {2}]", y, x, frameWidth);
+                }
+            }
+
+            return pixels;
         }
     }
 }
